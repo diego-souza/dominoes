59e8a56c1261ee746017e373d78c006decbe1230
Diego Alves de Souza <diegosouza.mail@gmail.com>
2015-07-01 11:14:58 -0300

    Runned transpec to change rspec syntax.

--- spec/dominoes/player_spec.rb
+++ spec/dominoes/player_spec.rb
@@ -3,153 +3,153 @@ describe Player do
   describe "basic attributes" do
     it "should have a name" do
       player = Player.new :name => "Diego"
-      player.name.should == "Diego"
+      expect(player.name).to eq("Diego")
     end
 
     it "should have a hand" do
-      hand = stub
+      hand = double
       player = Player.new :hand => hand
-      player.hand.should == hand 
+      expect(player.hand).to eq(hand) 
     end
 
     it "hand should be empty if not initialized" do
       player = Player.new
-      player.hand.should be_empty
+      expect(player.hand).to be_empty
     end
   end
 
   describe "receive stones" do
     it "should add stone to player's hand" do
       player = Player.new
-      stone = stub
+      stone = double
       stone.stub(:to_a => [stone])
       player.receive_stones stone
-      player.hand.should == [stone]
+      expect(player.hand).to eq([stone])
     end
 
     it "should add several stones to player's hand" do
       player = Player.new
-      stone = stub
-      other_stone = stub
+      stone = double
+      other_stone = double
       stones = [stone, other_stone]
       player.receive_stones stones
-      player.hand.should =~ [stone, other_stone]
+      expect(player.hand).to match_array([stone, other_stone])
     end
   end
 
   describe "give stone" do
     it "should remove stone from player's hand" do
       player = Player.new
-      stone = stub
+      stone = double
       stone.stub(:to_a => [stone])
       player.receive_stones [stone]
       player.give_stones stone
-      player.hand.should == []
+      expect(player.hand).to eq([])
     end
 
     it "should remove several stones from player's hand" do
       player = Player.new
-      stone = stub
-      other_stone = stub
+      stone = double
+      other_stone = double
       stones = [stone, other_stone]
       player.receive_stones stones
       player.give_stones stones
-      player.hand.should =~ []
+      expect(player.hand).to match_array([])
     end
   end
 
   describe "doubles" do
     it "should return all stones that are doubles" do
-      double_stone = stub(:double? => true)
-      regular_stone = stub(:double? => false)
+      double_stone = double(:double? => true)
+      regular_stone = double(:double? => false)
       stones = [double_stone, regular_stone]
       player = Player.new
       player.receive_stones stones
-      player.doubles.should == [double_stone]
+      expect(player.doubles).to eq([double_stone])
     end
 
     it "should return empty array if none stones are doubles" do
-      double_stone = stub(:double? => false)
-      regular_stone = stub(:double? => false)
+      double_stone = double(:double? => false)
+      regular_stone = double(:double? => false)
       stones = [double_stone, regular_stone]
       player = Player.new
       player.receive_stones stones
-      player.doubles.should == []
+      expect(player.doubles).to eq([])
     end
   end
 
   describe "has_double?" do
     it "should be true if any of its stones is double" do
-      stones = [stub(:double? => false), stub(:double? => true)]
+      stones = [double(:double? => false), double(:double? => true)]
       player = Player.new
       player.receive_stones stones
-      player.has_double?.should be_true
+      expect(player.has_double?).to be_truthy
     end
 
     it "should be false if none of its stones is double" do
-      stones = [stub(:double? => false), stub(:double? => false)]
+      stones = [double(:double? => false), double(:double? => false)]
       player = Player.new
       player.receive_stones stones
-      player.has_double?.should be_false
+      expect(player.has_double?).to be_falsey
     end
   end
 
   describe "max_double" do
     it "should return -1 if has no doubles" do
-      stones = [stub(:double? => false), stub(:double? => false)]
+      stones = [double(:double? => false), double(:double? => false)]
       player = Player.new
       player.receive_stones stones
-      player.max_double.should == -1
+      expect(player.max_double).to eq(-1)
     end
 
     it "should return biggest number of doubles if has doubles" do
-      stones = [stub(:double? => true, :first_number => 1, :second_number => 1), stub(:double? => true, :first_number => 2, :second_number => 2)]
+      stones = [double(:double? => true, :first_number => 1, :second_number => 1), double(:double? => true, :first_number => 2, :second_number => 2)]
       player = Player.new
       player.receive_stones stones
-      player.max_double.should == 2
+      expect(player.max_double).to eq(2)
     end
   end
 
   describe "has_stone?" do
     it "should return true if has stone" do
-      stone = stub
+      stone = double
       stones = [stone]
       player = Player.new
       player.receive_stones stones
-      player.has_stone?(stone).should == true
+      expect(player.has_stone?(stone)).to eq(true)
     end
 
     it "should return false if does not have stone" do
-      stone = stub
-      another_stone = stub
+      stone = double
+      another_stone = double
       stones = [another_stone]
       player = Player.new
       player.receive_stones stones
-      player.has_stone?(stone).should == false
+      expect(player.has_stone?(stone)).to eq(false)
     end
   end
 
   describe "has_matching_stone?" do
     it "should be false if stones in hand do not match any argument" do
-      number = stub
-      stone = mock
-      stone.should_receive(:matches?).with(number).and_return(false)
+      number = double
+      stone = double
+      expect(stone).to receive(:matches?).with(number).and_return(false)
       stones = [stone]
       player = Player.new
       player.receive_stones stones
-      player.has_matching_stone?(number).should == false
+      expect(player.has_matching_stone?(number)).to eq(false)
     end
 
     it "should be true if any stone in hand match any argument" do
-      number = stub
-      matching_stone = mock
-      stone = mock
-      matching_stone.should_receive(:matches?).with(number).and_return(true)
-      stone.should_receive(:matches?).with(number).and_return(false)
+      number = double
+      matching_stone = double
+      stone = double
+      expect(matching_stone).to receive(:matches?).with(number).and_return(true)
+      expect(stone).to receive(:matches?).with(number).and_return(false)
       stones = [stone, matching_stone]
       player = Player.new
       player.receive_stones stones
-      player.has_matching_stone?(number).should == true
+      expect(player.has_matching_stone?(number)).to eq(true)
     end
 
   end
--- spec/dominoes/playground_spec.rb
+++ spec/dominoes/playground_spec.rb
@@ -2,28 +2,28 @@ require 'dominoes/playground'
 describe Playground do
   it "should have empty area at the beginning" do
     playground = Playground.new
-    playground.area.should == []
+    expect(playground.area).to eq([])
   end
   describe "play" do
     it "should accept first stone if double" do
-      stone = stub(:double? => true, 
+      stone = double(:double? => true, 
                    :first_number => 6,
                    :second_number => 6)
       playground = Playground.new
       playground.play_stone stone
-      playground.area.should == [6, 6]
+      expect(playground.area).to eq([6, 6])
     end
 
     it "should raise error if first stone is not double" do
-      stone = stub(:double? => false) 
+      stone = double(:double? => false) 
       playground = Playground.new
-      lambda {playground.play_stone stone, :right}.should raise_error CantPlayStone
-      playground.area.should == []
+      expect {playground.play_stone stone, :right}.to raise_error CantPlayStone
+      expect(playground.area).to eq([])
     end
 
     context "when there is a first stone played" do
       before(:each) do
-        stone = stub(:double? => true, 
+        stone = double(:double? => true, 
                      :first_number => 6,
                      :second_number => 6)
         @playground = Playground.new
@@ -31,43 +31,43 @@ describe Playground do
       end
       context "last edge" do
         it "should accept if stone matches the edges with first number" do
-          stone = stub(:matches? => true,
+          stone = double(:matches? => true,
                        :first_number => 6,
                        :second_number => 5)
           @playground.play_stone stone, :last
-          @playground.area.should == [6, 6, 6, 5]
+          expect(@playground.area).to eq([6, 6, 6, 5])
         end
         it "should accept if stone matches the edges with second number" do
-          stone = stub(:matches? => true,
+          stone = double(:matches? => true,
                        :first_number => 5,
                        :second_number => 6)
           @playground.play_stone stone, :last
-          @playground.area.should == [6, 6, 6, 5]
+          expect(@playground.area).to eq([6, 6, 6, 5])
         end
       end
 
       context "first edge" do
         it "should accept if stone matches the edges with first number" do
-          stone = stub(:matches? => true,
+          stone = double(:matches? => true,
                        :first_number => 6,
                        :second_number => 5)
           @playground.play_stone stone, :first
-          @playground.area.should == [5, 6, 6, 6]
+          expect(@playground.area).to eq([5, 6, 6, 6])
         end
         it "should accept if stone matches the edges with second number" do
-          stone = stub(:matches? => true,
+          stone = double(:matches? => true,
                        :first_number => 5,
                        :second_number => 6)
           @playground.play_stone stone, :first
-          @playground.area.should == [5, 6, 6, 6]
+          expect(@playground.area).to eq([5, 6, 6, 6])
         end
       end
 
       context "does not match" do
         it "should raise error and not play stone" do
-          stone = stub(:matches? => false)
-          lambda { @playground.play_stone stone, :first }.should raise_error CantPlayStone
-          @playground.area.should == [6, 6]
+          stone = double(:matches? => false)
+          expect { @playground.play_stone stone, :first }.to raise_error CantPlayStone
+          expect(@playground.area).to eq([6, 6])
         end
       end
     end
